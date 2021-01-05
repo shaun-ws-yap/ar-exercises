0e09719844559bf72e00ce393b5e85b700706a24
Shaun Yap <me@shaunyap.ca>
2021-01-05 05:08:55 +0000

    complete exercise 7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,4 +1,8 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: {only_integer: true, less_than: 200, greater_than_or_equal_to: 40}
+  validates :store_id, presence: true
 end
 
